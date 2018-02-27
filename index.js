6bd2a2b017cbb95636d8b96d81a2c9ab56022d6d
Po Tsui <potsui@stanford.edu>
2018-02-27 02:50:30 -0800

    Set up Node, index pages and Procfile
    Fill in with dummy starter code to test that Heroku is deploying
    the Node app correctly

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index', {id: 0});
+});
+
+app.get('/:id', function(request, response) {
+    var id = request.params.id;
+    response.render('pages/index', {id: id});
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
