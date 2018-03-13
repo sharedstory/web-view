27ab932562ecbc3bd7794125211cb8a3584bc809
Po Tsui <potsui@stanford.edu>
2018-03-13 00:59:08 -0700

    Enclose initial data and export

--- initialData.js
+++ initialData.js
@@ -1,23 +1,39 @@
-var initialData = [
-	{
-		map: "stanford",
-		markers: [
-			{
-				text: "home",
-				x: 0.3521981,
-				y: 0.64898306,
-			},
-			{
-				text: "dining hall",
-				x: 0.54181635,
-				y: 0.3560664,
-			},
-			{
-				text: "work",
-				x: 0.3668223,
-				y: 0.3746807,
-			}
-		],
-		timestamp: Date.now(),
-	}
-];
+(function() {
+	var initialData = [
+		{
+			map: "stanford",
+			markers: [
+				{
+					text: "home",
+					x: 0.3521981,
+					y: 0.64898306,
+				},
+				{
+					text: "dining hall",
+					x: 0.54181635,
+					y: 0.3560664,
+				},
+				{
+					text: "work",
+					x: 0.3668223,
+					y: 0.3746807,
+				}
+			],
+			timestamp: Date.now(),
+		}
+	];
+
+	var models =  {
+		initialData: initialData,
+   };
+
+   if( typeof exports !== 'undefined' ) {
+	   // We're being loaded by the Node.js module loader ('require') 	so we use its
+	   // conventions of returning the object in exports.
+	   exports.models = models;
+   } else {
+	   // We're not in the Note.js module loader so we assume we're being loaded
+	   // by the browser into the DOM.
+	   window.models = models;
+   }
+})();
